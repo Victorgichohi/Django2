694cc4109eb6580ea6e9ec475b8f4e8e183e9a87
victorgichohi <victorgichohi48@gmail.com>
2015-10-14 12:29:41 +0300

    registration files and crispy forms

---
+++ envname/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/victor/Desktop/my_projects/Django/django2/envname/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- first_app/first_app/settings.py
+++ first_app/first_app/settings.py
@@ -35,9 +35,13 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    
+    'django.contrib.sites',
+    'registration',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'crispy_forms',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,7 +60,7 @@ ROOT_URLCONF = 'first_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':[os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,6 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR), "assets", "static_prod")
@@ -109,3 +114,6 @@ STATICFILES_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR), "assets", "static"),
 
 )
+
+ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
