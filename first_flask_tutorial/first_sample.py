f36c54ed3b66837eafa9925f85e0a38666e8217d
Victorgichohi <victorgichohi48@gmail.com>
2015-12-20 21:28:37 +0300

    accessing query variables

--- first_flask_tutorial/first_sample.py
+++ first_flask_tutorial/first_sample.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+from flask import request
 app=Flask(__name__)
 
 @app.route('/')
 def index(name="james"):
+    name=request.args.get('name',name)
     return "hello {}".format(name)
 
 app.run(debug=True , port=8000 , host='0.0.0.0')
