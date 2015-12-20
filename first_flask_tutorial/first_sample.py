b64d0edd483b4fa4f74042a19b95a8edb6caef64
Victorgichohi <victorgichohi48@gmail.com>
2015-12-20 21:58:07 +0300

    added some html inside the body of the function

--- first_flask_tutorial/first_sample.py
+++ first_flask_tutorial/first_sample.py
@@ -9,7 +9,18 @@ def index(name="james"):
     return "hello {}".format(name)
 
 @app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
 def add(num1,num2):
-    return "{}+{}={}".format(num1,num2,num1+num2)
+    return """
+    <!doctype html>
+    <head><title>some small rendering</title></head>
+    <body>
+
+    <h1>{}+{}={}</h1>
+    </body>
+    <html>""".format(num1,num2,num1+num2)
+
 
 app.run(debug=True , port=8000 , host='0.0.0.0')
