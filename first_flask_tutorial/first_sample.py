27503fcdce6a68a91c547dbc56f67053ca803888
Victorgichohi <victorgichohi48@gmail.com>
2015-12-20 22:30:00 +0300

    added templates files and can now render them dynamically

--- first_flask_tutorial/first_sample.py
+++ first_flask_tutorial/first_sample.py
@@ -1,26 +1,21 @@
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 @app.route('/<name>')
 def index(name="james"):
-    # name=request.args.get('name',name)
-    return "hello {}".format(name)
+    # context=request.args.get('name',name)
+    return render_template("index.html",name=name)
 
 @app.route('/add/<int:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1,num2):
-    return """
-    <!doctype html>
-    <head><title>some small rendering</title></head>
-    <body>
-
-    <h1>{}+{}={}</h1>
-    </body>
-    <html>""".format(num1,num2,num1+num2)
+    context={'num1':num1,'num2':num2}
+    return render_template("add.html",**context)
 
 
 app.run(debug=True , port=8000 , host='0.0.0.0')
