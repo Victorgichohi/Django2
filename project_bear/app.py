5606421270b2089e0e81c6767eb84c920817f71b
Victorgichohi <victorgichohi48@gmail.com>
2015-12-20 23:52:19 +0300

    these are the real project files for project bear

--- project_bear/app.py
+++ project_bear/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,url_for,redirect,request
 from flask import render_template
 
 app = Flask(__name__)
@@ -7,4 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/save',methods=['POST'])
+def save():
+    import pdb; pdb.set_trace()
+
+    return redirect (url_for('index'))
 app.run(debug=True, port=8000, host='0.0.0.0')
