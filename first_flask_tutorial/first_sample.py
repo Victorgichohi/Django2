da9b399acac00094c5b488dde0c6003cc39ead1e
Victorgichohi <victorgichohi48@gmail.com>
2015-12-20 21:46:10 +0300

    can add numbers on the browser dynamicaly

--- first_flask_tutorial/first_sample.py
+++ first_flask_tutorial/first_sample.py
@@ -1,10 +1,15 @@
 from flask import Flask
-from flask import request
+
 app=Flask(__name__)
 
 @app.route('/')
+@app.route('/<name>')
 def index(name="james"):
-    name=request.args.get('name',name)
+    # name=request.args.get('name',name)
     return "hello {}".format(name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1,num2):
+    return "{}+{}={}".format(num1,num2,num1+num2)
 
 app.run(debug=True , port=8000 , host='0.0.0.0')
