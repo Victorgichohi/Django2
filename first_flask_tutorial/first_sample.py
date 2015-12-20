276252ea5608ba5d9066d9ede171692b33da2017
Victorgichohi <victorgichohi48@gmail.com>
2015-12-20 21:22:36 +0300

    the parameter is in the function name and it can still renser on the browser

--- first_flask_tutorial/first_sample.py
+++ first_flask_tutorial/first_sample.py
@@ -3,7 +3,7 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-def index():
-    return "hello victor"
+def index(name="james"):
+    return "hello {}".format(name)
 
 app.run(debug=True , port=8000 , host='0.0.0.0')
